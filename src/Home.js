2a426209cb4cbe379171c88a6a75a562143f4547
Lakshmikiruthika2003 <Lakshmikiruthika2003>
2024-03-11 04:17:42 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to MEALS CORNER</h3>
+      <img className="home-img" src="https://media.istockphoto.com/id/682477410/photo/banana-leaf-rice.jpg?s=612x612&w=0&k=20&c=Gygl85axt5B9ZSioULTNDcBNcHeDB5p1RzOr4zIT7O0=" alt="Welcome picture" />
+    </div>
+  )
+}
